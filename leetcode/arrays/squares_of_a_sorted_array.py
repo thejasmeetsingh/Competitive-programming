0aab5cd94cafea90a5b461a5c48d9344f9f72244
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2020-12-12 00:33:35 +0530

    Solved leetcode Squares of a Sorted Array problem

---
+++ leetcode/arrays/squares_of_a_sorted_array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortedSquares(self, nums: list) -> list:
+        if not nums:
+            return nums
+
+        for i in range(len(nums)):
+            nums[i] = nums[i] ** 2
+
+        nums.sort()
+        return nums
