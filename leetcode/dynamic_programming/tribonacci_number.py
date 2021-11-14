0a71923abdde39659695aa23547aeb699b38567c
Jasmeet Singh <51269010+thejasmeetsingh@users.noreply.github.com>
2021-11-14 17:39:46 +0530

    Updated Tribonaaci Number Problem Logic

--- leetcode/dynamic_programming/tribonacci_number.py
+++ leetcode/dynamic_programming/tribonacci_number.py
@@ -1,11 +1,15 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        if not n:
+        if n == 0:
             return 0
+        if n == 1 or n == 2:
+          return 1
 
-        arr = [0, 1, 1]
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 1
 
-        for i in range(3, n + 1):
-            arr.append(arr[i - 1] + arr[i - 2] + arr[i - 3])
-
-        return arr[n]
+        for i in range(3, n+1):
+          dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+        
+        return dp[n]
