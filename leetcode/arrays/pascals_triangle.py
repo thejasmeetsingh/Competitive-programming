6fdb690059983da7095e360fddd8cdda87d41d42
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2021-12-03 22:48:48 +0530

    Updated Leetcode Pascals Triangle Problem Logic

--- leetcode/arrays/pascals_triangle.py
+++ leetcode/arrays/pascals_triangle.py
@@ -1,12 +1,20 @@
 class Solution:
-    def generate(self, num_rows: int) -> list:
-        if num_rows == 0:
+    def generate(self, numRows: int) -> list:
+        if not numRows:
             return []
 
-        triangle = [[1]]
+        if numRows == 1:
+            return [[1]]
 
-        for i in range(1, num_rows):
-            prev_row = triangle[i-1]
-            triangle.append([1] + list(map(lambda j: prev_row[j-1] + prev_row[j], range(1, i))) + [1])
+        triangle = [[1], [1, 1]]
+
+        for i in range(2, numRows):
+            row = []
+            for j in range(i + 1):
+                if j == 0 or j >= i:
+                    row.append(1)
+                elif j < len(triangle[-1]):
+                    row.append(triangle[-1][j - 1] + triangle[-1][j])
+            triangle.append(row)
 
         return triangle
