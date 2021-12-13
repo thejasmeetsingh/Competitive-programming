f82fc99199e64313b7ea11954558c5e83029333d
Jasmeet Singh <51269010+thejasmeetsingh@users.noreply.github.com>
2021-12-13 21:59:46 +0530

    Updated logic of Leetcode Majority Element Problem

--- leetcode/arrays/majority_element.py
+++ leetcode/arrays/majority_element.py
@@ -1,3 +1,11 @@
 class Solution:
-    def majorityElement(self, nums: list) -> int:
-        return sorted(nums)[len(nums)//2]
+    def majorityElement(self, nums: List[int]) -> int:
+        hmap = dict()
+        limit = len(nums) // 2
+        
+        for num in nums:
+          hmap[num] = hmap.get(num, 0) + 1
+          if hmap[num] > limit:
+            return num
+        
+        return 0
