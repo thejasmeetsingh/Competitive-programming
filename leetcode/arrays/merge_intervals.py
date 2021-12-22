042e3751ea77851ff964654a7cee2f2796d9cb5b
Jasmeet Singh <51269010+thejasmeetsingh@users.noreply.github.com>
2021-12-22 22:27:24 +0530

    Updated Logic of Leetcode Merge Intervals Problem

--- leetcode/arrays/merge_intervals.py
+++ leetcode/arrays/merge_intervals.py
@@ -1,13 +1,15 @@
 class Solution:
-    def merge(self, intervals: list) -> list:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+          return intervals
+        
+        result = list()
         intervals.sort(key=lambda x: x[0])
-
-        merged = []
-
+        
         for interval in intervals:
-            if not merged or merged[-1][1] < interval[0]:
-                merged.append(interval)
-            elif interval[1] > merged[-1][1]:
-                merged[-1][1] = interval[1]
-
-        return merged
+          if not result or result[-1][1] < interval[0]:
+            result.append(interval)
+          else:
+            result[-1][1] = max(result[-1][1], interval[1])
+        
+        return result
