7074ec601171dcb7009aac494cce599d2aa2820b
Jasmeet Singh <51269010+thejasmeetsingh@users.noreply.github.com>
2021-12-22 22:45:10 +0530

    Updated Logic of Leetcode Pascal's Triangle 2 Problem

--- leetcode/arrays/pascals_triangle_2.py
+++ leetcode/arrays/pascals_triangle_2.py
@@ -1,16 +1,23 @@
 class Solution:
-    def getRow(self, row_index: int) -> list:
-        if row_index == 0:
-            return [1]
-
-        triangle = [1] * (row_index + 1)
-
-        for i in range(1, row_index + 1):
-            start = triangle[0]
-            end = triangle[-1]
-            for j in range(1, i):
-                triangle[j] = start + end
-                start = end
-                end = triangle[j + 1]
-
-        return triangle
+    def getRow(self, rowIndex: int) -> List[int]:
+      triangle = [
+        [1],
+        [1, 1],
+        [1, 2, 1],
+        [1, 3, 3, 1],
+        [1, 4, 6, 4, 1],
+      ]
+      
+      if rowIndex <= 4:
+        return triangle[rowIndex]
+      
+      for i in range(5, rowIndex + 1):
+        new_triangle = [1]
+        
+        for j in range(1, i):
+          new_triangle.append(triangle[-1][j - 1] + triangle[-1][j])
+        
+        new_triangle.append(1)
+        triangle.append(new_triangle)
+      
+      return triangle[rowIndex]
