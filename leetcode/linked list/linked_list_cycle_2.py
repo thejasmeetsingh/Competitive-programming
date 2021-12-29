441d9494806ce6d374c47e76ac56424d41c343ec
Jasmeet Singh <51269010+thejasmeetsingh@users.noreply.github.com>
2021-12-29 21:21:20 +0530

    Solved Leetcode Linked List Cycle 2 Problem

---
+++ leetcode/linked list/linked_list_cycle_2.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        slow = fast = head
+
+        while slow and fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            # If slow==fast, a loop is detected. Now find start of the loop.
+            if slow == fast:
+                fast = head
+
+                while fast is not slow:
+                    slow = slow.next
+                    fast = fast.next
+                return slow
+
+        return None
