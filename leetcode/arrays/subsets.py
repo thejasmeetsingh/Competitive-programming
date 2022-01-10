aa30b89ddf3a7c91add6314e944c53da36143d1e
Jasmeet Singh <51269010+thejasmeetsingh@users.noreply.github.com>
2022-01-10 21:02:01 +0530

    Updated Leetcode Subset problem Logic

--- leetcode/arrays/subsets.py
+++ leetcode/arrays/subsets.py
@@ -1,13 +1,12 @@
 class Solution:
-    def subsets(self, nums: list) -> list:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
         if not nums:
-            return []
-
-        n = len(nums)
-        output = []
-
-        for i in range(2 ** n, 2 ** (n + 1)):
-            bitmask = bin(i)[3:]
-            output.append([nums[j] for j in range(n) if bitmask[j] == '1'])
-
-        return output
+          return nums
+        
+        result = [[]]
+        
+        for num in nums:
+          result += [sub_arr + [num] for sub_arr in result]
+      
+        return result
+        
